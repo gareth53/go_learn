12dd3494ad2ed219bc28247550256fec7ba05c0c
Gareth Senior <gareth.senior@sainsburys.co.uk>
2016-06-03 15:54:58 +0100

    02 file -playing with var declrataions02 file -playing with var declrataions initial idea of the S3 interface

--- 02_cli_args.go
+++ 02_cli_args.go
@@ -4,6 +4,7 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
 )
 
 /*
@@ -11,7 +12,7 @@ to run:
 > go run 02_cli_args.go foo bar fubar fugazi
 */
 
-func main() {
+func method1() {
     var s, sep string
     for i :=1; i < len(os.Args); i++ {
         s += sep + os.Args[i]
@@ -27,3 +28,34 @@ and that strings have a default value
 the := is a short variable declaration
 the type is inferred from the initial value
 */
+
+func method2() {
+    s, sep := "", ""
+    for _, arg := range os.Args[1:] {
+        s += sep + arg
+        sep = ", "
+    }
+    fmt.Println(s)
+}
+/*
+in method 2 we use the short var declaration to decalre two var values
+we're using range too, which is simialr to Python's enumerate()
+note: _ is a special variable name (a blank identifier) that cannot be used
+if we had used a var name like 'temp' we would encounter a compiler error
+the Go compiler HATE unused variables :)
+*/
+
+func method3() {
+    fmt.Println(strings.Join(os.Args[1:], ", "))
+}
+/*
+method3 func is the proper way of doing things
+but had we jumped straight to this technique
+we'd have not learned all that stuff about var declaration or blank identifiers or the use of range
+*/
+
+func main() {
+    method1()
+    method2()
+    method3()
+}
