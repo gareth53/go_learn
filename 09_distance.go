37db6a17bfa66a3b967b575f7a35d88d5b3110c1
Gareth Senior <gareth.senior@sainsburys.co.uk>
2016-07-08 17:12:05 +0100

    converting CM to inches (and some strign formatting niceties)

---
+++ 09_distance.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "strconv"
+)
+
+type Inch float64
+type Centimetre float64
+
+func(i Inch) String() string {
+  inches := uint(i) % 12
+  feet := (uint(i) - inches) / 12
+  if feet > 0 {
+    return fmt.Sprintf("%d'%d\"", feet, inches)
+  }
+  return fmt.Sprintf("%d\"", inches)
+}
+
+func(c Centimetre) String() string {
+  cm := uint(c) % 100
+  m := (uint(c) - cm) / 100.0
+  if m > 0 {
+    return fmt.Sprintf("%.2fm", c/100)
+  }
+  return fmt.Sprintf("%dcm", uint(c))
+}
+
+func CmToI(c Centimetre) Inch {
+  return Inch(c/2.49)
+}
+
+func main() {
+  num, err := strconv.Atoi(os.Args[1])
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  cm := Centimetre(num)
+  fmt.Println(cm)
+  fmt.Println(CmToI(cm))
+}
