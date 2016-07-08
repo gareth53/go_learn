055df9c029c37efc721dcca7623ef8da57113a48
Gareth Senior <gareth.senior@sainsburys.co.uk>
2016-07-08 08:35:17 +0100

    remove commented-out code

--- 03b_dup_lins_multifiles.go
+++ 03b_dup_lins_multifiles.go
@@ -10,7 +10,7 @@ func main() {
   count := make(map[string]int)
   occurrence := make(map[string]string)
   if len(os.Args) < 2 {
-    fmt.Println("ERROR! Yuo must specify files to be analysed")
+    fmt.Println("ERROR! You must specify files to be analysed")
   }
   for _, filepath := range os.Args[1:] {
       file, _ := os.Open(filepath)
@@ -20,16 +20,6 @@ func main() {
         occurrence[input.Text()] += " " + filepath
       }
   }
-  /*
-  input_file, err := os.Open(input_filepath)
-  if err != nil {
-    fmt.Printf("Error! %s\n", err)
-  }
-  input := bufio.NewScanner(input_file)
-  for input.Scan() {
-    count[input.Text()]++
-  }
-  */
   for line, n := range count {
     if n > 1 {
       fmt.Printf("%d\t%s (%s)\n", n, line, occurrence[line])
