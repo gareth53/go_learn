96151b85f6d8d9b1002cfbbbda2b571a04cad07d
Gareth Senior <gareth.senior@sainsburys.co.uk>
2016-08-03 15:18:19 +0100

    slightly pithier code

--- ch4/04_eliminating_repeated_chars.go
+++ ch4/04_eliminating_repeated_chars.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func remove_dupes(str string) string {
   output := str[0:1]
-  prev := str[:1]
   var char string
   for i := 1; i<len(str); i++ {
     char = str[i:i+1]
-    if char != prev {
+    if char != output[len(output)-1:] {
       output += char
-      prev = char
     }
   }
   return output
